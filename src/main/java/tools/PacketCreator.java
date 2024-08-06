2ff9c65f479b399dbb93eba8613fb6b55bd3e75b
Marlo Delatorre <delatormc@gmail.com>
2024-08-06 02:48:53 -0700

    Update PacketCreator.java

--- src/main/java/tools/PacketCreator.java
+++ src/main/java/tools/PacketCreator.java
@@ -907,7 +907,7 @@ public class PacketCreator {
             addCharEntry(p, chr, false);
         }
 
-	p.writeByte(2);
+	p.writeByte(2); // Disable PIC
         p.writeInt(YamlConfig.config.server.COLLECTIVE_CHARSLOT ? chars.size() + c.getAvailableCharacterSlots() : c.getCharacterSlots());
         return p;
     }
